76b6916e24cc49118363daff2e697fb1c88441a9
asadahmed01 <asadatnish@gmail.com>
2022-05-03 21:46:09 -0400

    optimized solution for two sum

--- Arrays/two_sum.py
+++ Arrays/two_sum.py
@@ -1,6 +1,6 @@
 from operator import indexOf
 
-
+# brute force solution
 def two_sum(nums, target):
     for i in range(len(nums) - 1):
         for j in range(i + 1, len(nums)):
@@ -8,7 +8,22 @@ def two_sum(nums, target):
                 print([nums[i], nums[j]])
 
 
-# nums = [2, 7, 11, 15]
-nums = [3, 2, 4]
-target = 6
+nums = [2, 7, 11, 15]
+
+# optimized solution
+def towSum_optimized(nums, target):
+    seen = {}
+    for i, n in enumerate(nums):
+        complement = target - n
+        print(complement)
+        if complement in seen:
+            print(seen)
+            return [nums[complement], i]
+        else:
+            seen[nums[i]] = n
+
+
+# nums = [3, 2, 4]
+target = 9
 two_sum(nums, target)
+print(towSum_optimized(nums, 9))

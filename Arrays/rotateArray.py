55f204d49a8f8a6e70c6dcf9e880bc5834eb4dee
asadahmed01 <asadatnish@gmail.com>
2022-05-07 09:11:35 -0400

    skip the number 13 because it is unlucky

--- Arrays/rotateArray.py
+++ Arrays/rotateArray.py
@@ -16,4 +16,16 @@ def count_evens(nums):
     return count
 
 
-print(count_evens([2, 1, 2, 3, 4]))
+# print(count_evens([2, 1, 2, 3, 4]))
+
+# Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
+def sum13(nums):
+    sum = 0
+    for i in range(len(nums)):
+        if nums[i] == 13:
+            break
+        sum += nums[i]
+    return sum
+
+
+print(sum13([1, 2, 2, 1, 13]))

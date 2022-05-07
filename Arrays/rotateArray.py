ab9bf15fc5e7a06d68da55d35a0729d596d01ed1
asadahmed01 <asadatnish@gmail.com>
2022-05-07 09:52:09 -0400

    sum67 solution

--- Arrays/rotateArray.py
+++ Arrays/rotateArray.py
@@ -28,4 +28,20 @@ def sum13(nums):
     return sum
 
 
-print(sum13([1, 2, 2, 1, 13]))
+# print(sum13([1, 2, 2, 1, 13]))
+
+# Return the sum of the numbers in the array, except ignore sections of numbers starting with a 6 and extending to the next 7 (every 6 will be followed by at least one 7). Return 0 for no numbers.
+def sum67(nums):
+    sum = 0
+    skip = False
+    for i in range(len(nums)):
+        if nums[i] == 6:
+            skip = True
+        elif skip == True and nums[i] == 7:
+            skip = False
+        elif skip == False:
+            sum += nums[i]
+    return sum
+
+
+print(sum67([1, 2, 2, 6, 99, 99, 7]))

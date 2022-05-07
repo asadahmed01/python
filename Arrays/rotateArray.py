312d3ba03fb6cf114ddf816e984b8bc64227d15c
asadahmed01 <asadatnish@gmail.com>
2022-05-07 08:53:35 -0400

    add solution that counts the number of evens in an list

--- Arrays/rotateArray.py
+++ Arrays/rotateArray.py
@@ -3,4 +3,17 @@ def rotate_left(nums):
     return nums[1:] + nums[:1]
 
 
-print(rotate_left([1, 2, 3]))
+# print(rotate_left([1, 2, 3]))
+
+# Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
+
+
+def count_evens(nums):
+    count = 0
+    for i, n in enumerate(nums):
+        if n % 2 == 0:
+            count += 1
+    return count
+
+
+print(count_evens([2, 1, 2, 3, 4]))

49f78f9a9826bf9630865b6850b746964d29b2e2
asadahmed01 <asadatnish@gmail.com>
2022-06-05 17:25:48 -0400

    add deposit method

---
+++ OOP/BankAccount.py
@@ -0,0 +1,17 @@
+from cmath import acos
+
+
+class BankAccount:
+    def __init__(self, accountNumber: int, name: str, balance: float):
+        self.accountNumber = accountNumber
+        self.name = name
+        self.balance = balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+
+
+accnt = BankAccount(1122, "john", 100.00)
+accnt.deposit(50.25)
+print(accnt.balance)

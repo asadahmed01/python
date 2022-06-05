d6f3d20de036f594e5e3ee5961b29f6093dfbb20
asadahmed01 <asadatnish@gmail.com>
2022-06-05 17:15:59 -0400

    add getters and setters

--- OOP/person.py
+++ OOP/person.py
@@ -6,6 +6,19 @@ class Person:
     def greet(self):
         print(f"Hi, my name is {self.name}")
 
+    # getter
+    @property
+    def getName(self):
+        return self.name
+
+    # setter
+    @changeAge.setter
+    def changeAge(self, a):
+        self.age = a
+
 
 asad = Person("assad", 30)
+print(asad)
 asad.greet()
+# asad.age = 25
+print(asad.age)

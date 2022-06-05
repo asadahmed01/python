42587b079321cd973c4d6ae92d1cf9ee439116db
asadahmed01 <asadatnish@gmail.com>
2022-06-05 17:31:19 -0400

    add withdraw method

--- OOP/BankAccount.py
+++ OOP/BankAccount.py
@@ -11,7 +11,15 @@ class BankAccount:
         if amount > 0:
             self.balance += amount
 
+    def withdraw(self, amount):
+        if self.balance < amount:
+            print("insufficient funds! your balance is $", self.balance)
+        elif self.balance >= amount:
+            self.balance -= amount
+
 
 accnt = BankAccount(1122, "john", 100.00)
 accnt.deposit(50.25)
+print(accnt.balance)
+accnt.withdraw(150)
 print(accnt.balance)

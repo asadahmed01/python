67c2f4dcc340a2fd77fd172cddad92ec0bfd77bd
asadahmed01 <asadatnish@gmail.com>
2022-06-06 10:15:03 -0400

    create the Employee class that will be the parent

---
+++ OOP/employee.py
@@ -0,0 +1,19 @@
+class Employee:
+    raise_amount = 1.04
+
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+    def fullname(self):
+        return f"{self.first} {self.last}"
+
+    def apply_raise(self):
+        self.pay = float(self.pay * self.raise_amount)
+
+
+emp1 = Employee("Asad", "Ahmed", 65000)
+emp1.apply_raise()
+print(emp1.fullname())
+print(emp1.pay)

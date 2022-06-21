81d8a5b1ff022578dda33ac05d0c8375d8d7f136
asadahmed01 <asadatnish@gmail.com>
2022-06-21 15:41:51 -0400

    add two pointer solution for the linkedlist palindrome

--- Linked List/palindrome_linkedlist.py
+++ Linked List/palindrome_linkedlist.py
@@ -8,21 +8,47 @@ class ListNode:
 
 
 class Solution:
+    # def isPalindrome(self, head: ListNode) -> bool:
+    #     # convert the linkedlist to array
+    #     nums = []
+
+    #     while head:
+    #         nums.append(head.val)
+    #         head = head.next
+
+    #     # check if it is palindrome
+    #     l = 0
+    #     r = len(nums) - 1
+    #     while l <= r:
+    #         if nums[l] != nums[r]:
+    #             return False
+    #         l += 1
+    #         r -= 1
+
+    #     return True
     def isPalindrome(self, head: ListNode) -> bool:
-        # convert the linkedlist to array
-        nums = []
+        fast = head
+        slow = head
+        # find the middle of the list
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
 
-        while head:
-            nums.append(head.val)
-            head = head.next
+        # reverse the second half of the list
+        prev = None
+        while slow:
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
 
-        # check if it is palindrome
-        l = 0
-        r = len(nums) - 1
-        while l <= r:
-            if nums[l] != nums[r]:
+        # check for palindrome
+        left = head
+        right = prev
+        while right:
+            if left.val != right.val:
                 return False
-            l += 1
-            r -= 1
+            right = right.next
+            left = left.next
 
         return True

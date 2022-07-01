efbc983c7edbcd0859992f352a620548046c6c5d
asadahmed01 <asadatnish@gmail.com>
2022-07-01 01:32:42 -0400

    add brute force solution for Valid Perfect Square

---
+++ BinarySearch/validPerfectSquare.py
@@ -0,0 +1,23 @@
+def isPerfectSquare(num) -> bool:
+    # Brute force soluton (loop)
+    # for i in range(1, num + 1):
+    #     if i * i == num:
+    #         return True
+    #     if i * i > num:
+    #         return False
+
+    # optimized solution (binary search)
+    start = 1
+    end = num
+    while start <= end:
+        mid = start + end // 2
+        if mid * mid == num:
+            return True
+        if mid * mid < num:
+            end = mid - 1
+        if mid * mid > num:
+            start = mid + 1
+    return False
+
+
+print(isPerfectSquare(9))

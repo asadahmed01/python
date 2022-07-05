0e7da02fe92063fccd0427f160bf1201eb1a206b
asadahmed01 <asadatnish@gmail.com>
2022-07-05 19:28:44 -0400

    find the right row in which the target value lies

--- BinarySearch/search2DArray.py
+++ BinarySearch/search2DArray.py
@@ -1,8 +1,19 @@
-def search2D(matrix: list[list[int]]) -> bool:
-    for i in range(len(matrix)):
-        print(matrix[i])
+def search2D(matrix: list[list[int]], target) -> bool:
+    # if matrix is empty
+    if not matrix or not matrix[0]:
+        return False
+    # now look for the row in which our target lies
+    row = 0
+
+    for r in range(len(matrix)):
+        if target == matrix[r][0] or target == matrix[r][-1]:
+            return True
+        elif target > matrix[r][0] and target < matrix[r][-1]:
+            row = r
+            break
+    # now do binary search on that row
 
 
 matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-search2D(matrix)
+search2D(matrix, 3)
